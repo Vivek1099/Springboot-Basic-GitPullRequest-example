8df479f11f40d44d3a83e7e461e3348ec8a1eb5f
Roshan kumar <RoshanKumarbaitha>
2024-09-09 17:19:08 +0530

    updated test and save

--- src/main/java/Springboot_Basic_GitPullRequest_example/Controller/StudentController.java
+++ src/main/java/Springboot_Basic_GitPullRequest_example/Controller/StudentController.java
@@ -1,4 +1,27 @@
 package Springboot_Basic_GitPullRequest_example.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Springboot_Basic_GitPullRequest_example.Entity.Student;
+import Springboot_Basic_GitPullRequest_example.Repository.StudentRepository;
+
+@RestController
 public class StudentController {
+	@Autowired
+	StudentRepository srepo;
+	@RequestMapping("/test")
+	public String test() {
+		return"test from Roshan";	
+	}
+
+@PostMapping("/save")
+public String saveData(@RequestBody Student s)
+{
+	srepo.save(s);
+	return"data is saved into database";
+}
 }
