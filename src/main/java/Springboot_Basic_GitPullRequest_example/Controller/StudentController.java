ef0f940738a44ce9f5cb4d613392ae67a5614691
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-09-09 16:53:39 +0530

    updated controller

--- src/main/java/Springboot_Basic_GitPullRequest_example/Controller/StudentController.java
+++ src/main/java/Springboot_Basic_GitPullRequest_example/Controller/StudentController.java
@@ -1,4 +1,18 @@
 package Springboot_Basic_GitPullRequest_example.Controller;
 
+import Springboot_Basic_GitPullRequest_example.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("/test")
+    public String test(){
+        return "This Is Koushal test";
+    }
 }
